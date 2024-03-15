add gradient clipping to training loop and slowed lr to fix exploding gradients

evoVAE/models/convoVAE.py
@@ -24,7 +24,7 @@ class ConvoVAE(BaseVAE):
         self.latentDims = latentDims
 
         if hiddenDims is None:
-            hiddenDims = [4, 8]
+            hiddenDims = [32, 64, 128, 256, 512]
 
         ### ENCODER ###
 
@@ -78,8 +78,8 @@ class ConvoVAE(BaseVAE):
                         stride=1,
                     ),
                     nn.Dropout(0.3),
+                    nn.BatchNorm2d(hiddenDims[i + 1]),
                     nn.LeakyReLU(),
-                    nn.BatchNorm2d(h_dim),
                 )
             )
 
@@ -89,14 +89,18 @@ class ConvoVAE(BaseVAE):
 
         self.final_layer = nn.Sequential(
             nn.ConvTranspose2d(
-                in_channels=hiddenDims[-1],
-                out_channels=hiddenDims[-1],
+                hiddenDims[-1],
+                hiddenDims[-1],
                 kernel_size=3,
-                stride=1,
+                stride=3,
+                padding=1,
+                output_padding=1,
             ),
+            # (Hin −1)×stride[0]−2×padding[0]+dilation[0]×(kernel_size[0]−1)+output_padding[0]+
             nn.BatchNorm2d(hiddenDims[-1]),
             nn.LeakyReLU(),
-            nn.Conv2d(hiddenDims[-1], out_channels=1, kernel_size=3),
+            # use the formula 28 = (20 - Kernal - 2Padding)/ Stride + 1
+            nn.Conv2d(hiddenDims[-1], out_channels=1, kernel_size=2),
             nn.Tanh(),
         )
 
@@ -113,7 +117,7 @@ class ConvoVAE(BaseVAE):
 
         return qPhi.rsample()
 
-    def encode(self, rawInput: Tensor) -> Tensor:
+    def encode(self, rawInput: Tensor) -> Tuple[Tensor, Tensor, torch.Size]:
         """Take the rawInput and encode into a bottle neck dimension
         specified by the encoder. Transform this into Mu and log variance
         values to construct a distribution. i.e. P_phi(Z|X)
@@ -121,34 +125,42 @@ class ConvoVAE(BaseVAE):
 
         # find qPhi encoding of the raw input
         encoderOutput = self.encoder(rawInput)
+        # save this for later so that we can resize out flattened tensor
+        shape = encoderOutput.shape
+
         encoderOutput = torch.flatten(encoderOutput, start_dim=1)
 
         # sample out the gaussian parameters
         zMu = self.zMuSampler(encoderOutput)
         zLogvar = self.zLogvarSampler(encoderOutput)
 
-        return zMu, zLogvar
+        return zMu, zLogvar, shape
 
-    def decode(self, zSample: Tensor) -> Tensor:
+    def decode(self, upscaledZ: Tensor) -> Tensor:
         """Sample a latent vector, Z, and
         learn the parameters from which reconstructions
         can be sampled from. I.e. we are learning P_sigma(X|Z)
         """
-        upscaledZ = self.decoder_input(zSample)
+
         xHat = self.decoder(upscaledZ)
+
         xHat = self.final_layer(xHat)
 
         return xHat
 
     def forward(self, rawInput: Tensor) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
 
-        zMu, zLogvar = self.encode(rawInput=rawInput)
+        zMu, zLogvar, shape = self.encode(rawInput=rawInput)
 
         # construct a gaussian distribution and sample
         zSample = self.reparameterise(zMu, zLogvar)
 
+        upscaledZ = self.decoder_input(zSample)
+
+        upscaledZ = upscaledZ.view(shape)
+
         # learn parameters for q_theta P(x|z)
-        xHat = self.decode(zSample)
+        xHat = self.decode(upscaledZ)
 
         return xHat, zSample, zMu, zLogvar
 

evoVAE/trainer/checkerboard_train.py
@@ -21,40 +21,42 @@ def train(model: BaseVAE, trainLoader, epochs=10, print_freq=10) -> BaseVAE:
     """Training cycle for checkboard dataset"""
 
     use_cuda = torch.cuda.is_available()
-    device = torch.device("cuda:0" if use_cuda else "cpu")
+    device = torch.device("cuda" if use_cuda else "cpu")
 
-    # speeds up if input size isn't changing 
+    # speeds up if input size isn't changing
     torch.backends.cudnn.benchmark = True
 
-    model.to(device)
-
+    model = model.to(device)
 
     start = time.time()
-    optimiser = model.configure_optimiser()
-    log = ""
+    optimiser = model.configure_optimiser(learningRate=1e-5)
 
     for iteration in range(epochs):
 
         batchSize = len(trainLoader)
         epochLoss = 0
         model.train(True)
-        
-        log += f"Epoch {iteration+1}\n-------------------------------\n"
+
+        print(f"Epoch {iteration+1}\n-------------------------------\n")
 
         for batch in trainLoader:
 
             data, _ = batch
-            data = data.reshape(-1, 28 * 28).to(device)
+            data = data.to(device)
+
+            optimiser.zero_grad()
 
             # forward step
             modelOutputs = model(data)
 
             # calculate loss
             loss, kl, likelihood = model.loss_function(modelOutputs, data)
+            print(loss)
             epochLoss += loss
 
-            optimiser.zero_grad()
             loss.backward()
+            # sets max value for gradient - currently 1.0
+            torch.nn.utils.clip_grad_norm(model.parameters(), 1.0)
             optimiser.step()
 
             if iteration % print_freq == 0:
@@ -63,12 +65,10 @@ def train(model: BaseVAE, trainLoader, epochs=10, print_freq=10) -> BaseVAE:
                     stats = print_progress(time.time() - start, iteration + 1, epochs)
                     batchLoss = f"Avg batch loss: {epochLoss/batchSize}\n"
 
-                    log += itera
-                    log += stats
-                    log += batchLoss
+                    print(itera)
+                    print(stats)
+                    print(batchLoss)
 
-    print(log)
     torch.save(model.state_dict(), "model_weights.pth")
 
     return model
-

run_vae.py
@@ -3,9 +3,7 @@ import torch
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 from evoVAE.trainer.checkerboard_train import train
-from evoVAE.decoders.minimal_decoder import MinDecoder
-from evoVAE.encoders.minimal_encoder import MinEncoder
-from evoVAE.models.standardVAE import StandardVAE
+from evoVAE.models.convoVAE import ConvoVAE
 
 
 # %%
@@ -24,25 +22,8 @@ test_loader = torch.utils.data.DataLoader(test_data, batch_size=128)
 
 # %%
 # standard input size for MINST data
-inputDim = 28 * 28
-
-bottleNeckDim = 512
-latentDim = 2
-
-encoder = MinEncoder(inputDim=inputDim, bottleNeckDim=bottleNeckDim)
-decoder = MinDecoder(
-    inputDim=bottleNeckDim, bottleNeckDim=bottleNeckDim, outputDim=inputDim
-)
-
-model = StandardVAE(
-    inputDims=inputDim,
-    bottleNeckDim=bottleNeckDim,
-    latentDim=latentDim,
-    encoder=encoder,
-    decoder=decoder,
-)
-
+model = ConvoVAE(in_channels=1, latentDims=10)
 
 # %%
-trained_model = train(model, train_loader, epochs=30, print_freq=10)
-torch.save(trained_model.state_dict(), 'model_weights.pth')
+trained_model = train(model, train_loader, epochs=40, print_freq=10)
+torch.save(trained_model.state_dict(), "model_weights.pth")