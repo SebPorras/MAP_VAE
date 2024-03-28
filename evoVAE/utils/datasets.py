7dc1d0604a8a2933407a4c61bae0801a143b8086
Sebastian Porras (qris-uq) <s4646506@bun098.hpc.net.uq.edu.au>
2024-03-28 18:15:15 +1000

    adds dataloader for DMS data and pipeline for processing DMS data

--- evoVAE/utils/datasets.py
+++ evoVAE/utils/datasets.py
@@ -22,3 +22,25 @@ class MSA_Dataset(Dataset):
 
     def __getitem__(self, index) -> Tuple[torch.tensor, float, str]:
         return self.encodings[index, :], self.weights[index], self.ids[index]
+
+class DMS_Dataset(Dataset):
+    """Hold data for a deep mutational scanning dataset"""
+    
+    def __init__(
+        self, encodings: np.ndarray, weights: np.ndarray, ids: np.ndarray, 
+        fitness: np.ndarray) -> None: 
+        
+        assert encodings.shape[0] == len(ids)
+        assert encodings.shape[0] == len(fitness)
+        
+        self.encodings = torch.tensor(np.stack(encodings))
+        self.ids = ids
+        self.fitness = torch.tensor(fitness.values)
+        
+    def __len__(self):
+        return self.encodings.shape[0]
+
+    def __getitem__(self, index) -> Tuple[torch.tensor, float, str]:
+        return self.encodings[index, :],  self.fitness[index], self.ids[index],
+        
+    
