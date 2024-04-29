963646207a1309cb7c393b1d571ecc1b67a6f596
sebs-mac <sebastian.porras01@gmail.com>
2024-04-30 07:40:49 +1000

    adds ancestor node sampling to seq_tools.py

--- evoVAE/utils/seq_tools.py
+++ evoVAE/utils/seq_tools.py
@@ -7,12 +7,13 @@ before they are passed to the VAE.
 import numpy as np
 from typing import Tuple, Dict, List
 import pandas as pd
-import torch, re, math
 import evoVAE.utils.metrics as mt
 from scipy.spatial import distance_matrix
 from scipy.spatial.distance import euclidean
 import matplotlib.pyplot as plt
-from Bio import Align
+from ete3 import Tree
+
+import torch, re, math, random
 
 
 GAPPY_PROTEIN_ALPHABET = [
@@ -362,3 +363,56 @@ def population_profile_deviation(
     mean_deviation = total_dist / sample_n
 
     return mean_deviation
+
+
+def sample_ancestor_nodes(
+    rejection_threshold: float, tree: Tree, tree_run: int
+) -> Tuple[List[str], int]:
+    """Do a level order traversal of a tree, at each ancestor, only sample if a random number
+    between 0 and 1 is below 1 - rejection_threshold.
+
+    Returns:
+    list of node names, the number of nodes sampled
+    """
+
+    sampled_nodes = []
+    for node in tree.traverse():
+
+        # we're only interested in sampling ancestors
+        if node.is_leaf():
+            continue
+
+        if random.random() <= 1 - rejection_threshold:
+            # pkl file format
+            sampled_nodes.append(node.name + f"_tree_{tree_run}")
+
+    return sampled_nodes, len(sampled_nodes)
+
+
+def sample_ancestor_trees(
+    tree_count: int,
+    rejection_threshold: float,
+    tree_path: str,
+    ancestor_pkl_path: str,
+    ete_read_setting: int = 1,
+):
+    """
+    Take a path to where all trees are stored. For each tree, sample the ancestor nodes, only taking ancestors and only sampling
+    when a random number is less than 1 - rejection_threshold.
+
+    Returns:
+    A dataframe with all the sampled_ancestors.
+    """
+
+    sampled_names = []
+    for iteration in range(1, tree_count + 1):
+
+        # default read setting is 1, to allow internal nodes to be read
+        t = Tree(tree_path + f"run_{iteration}_ancestors.nwk", ete_read_setting)
+        names, _ = sample_ancestor_nodes(rejection_threshold, t, iteration)
+        sampled_names += names
+
+    sampled_ancestors = pd.read_pickle(ancestor_pkl_path)
+    sampled_ancestors.loc[sampled_ancestors["id"].isin(sampled_names)]
+
+    return sampled_ancestors
