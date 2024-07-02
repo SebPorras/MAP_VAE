ef1d1c8ad8642b6692f24793384b97bf76159f39
Sebastian Porras (qris-uq) <s4646506@bun098.hpc.net.uq.edu.au>
2024-07-02 21:20:17 +1000

    random seq gen scripts

---
+++ scripts/make_configs/gcn4/gcn4_curated_config.py
@@ -0,0 +1,38 @@
+import yaml
+
+groups = ["ancestors_extants", "ancestors", "extants"]
+ids = ["ae", "a", "e"]
+data = ["gcn4_0.3_sim_ancestors_0.3_sim_extants_no_dupes.aln", 
+        "gcn4_0.3_sim_ancestors_no_dupes.aln",
+        "gcn4_0.3_sim_extants_no_dupes.aln"]
+
+
+for g, id, d, in zip(groups, ids, data):
+
+    data = {
+        "alignment": f"/scratch/user/s4646506/gcn4/curated/alns/{d}",
+        "extant_aln": "/scratch/user/s4646506/gcn4/encoded_weighted/gcn4_extants_no_dupes.pkl",
+        "AA_count": 21,
+        "info": "./gcn4_" + id,
+        "project": None,
+        "seq_theta": 0.2,  # reweighting,
+        "test_split": 0.2,
+        "max_mutation": 4,  # how many mutations the model will test up to,
+        "learning_rate": 0.0001,  # ADAM,
+        "weight_decay": 0,  # ADAM,
+        "epochs": 300,
+        "batch_size": 128,
+        "patience": 3,
+        "architecture": "SeqVAEv2",
+        "latent_dims": 3,
+        "hidden_dims": [150, 150],
+        "dms_file": "/scratch/user/s4646506/gcn4/dms_data/GCN4_YEAST_Staller_2018.csv",
+        "dms_metadata": "/scratch/user/s4646506/evoVAE/data/DMS_substitutions.csv",
+        "dms_id": "GCN4_YEAST_Staller_2018",
+        "replicate_csv": None,
+    }
+
+    yaml_str = yaml.dump(data, default_flow_style=False)
+
+    with open(f"gcn4_{g}_config.yaml", "w") as file:
+        file.write(yaml_str)
---
+++ scripts/random_seq_gen.py
@@ -0,0 +1,17 @@
+import evoVAE.utils.seq_tools as st
+import random
+import pandas as pd
+
+seq_count = 10000
+seq_len = 448
+seqs = []
+ids = []
+count = 0
+for i in range(1, seq_count+1):
+    rand_seq = "".join([st.IDX_TO_AA[random.randint(0, 20)] for x in range(seq_len)])
+    seqs.append(rand_seq)
+    ids.append(f"{i}_rand")
+ 
+df = pd.DataFrame({"id": ids, "sequence": seqs})
+st.write_fasta_file("gb1_random_seqs.aln", df)
+
